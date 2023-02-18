a7ee0f332889f8afdf762e62583320d9efa1f201
sufiyanirfan3 <sufiyanirfan1613@gmail.com>
2023-02-18 22:36:53 +0500

    .

--- server/server.js
+++ server/server.js
@@ -29,7 +29,7 @@ app.post('/chat', async (req, res) => {
       model: "text-curie-001",
       prompt: `${question}`,
       temperature: 0.7, // Higher values means the model will take more risks.
-      max_tokens: 2048, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
+      max_tokens: 1536, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
       top_p: 1, // alternative to sampling with temperature, called nucleus sampling
       frequency_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
       presence_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
